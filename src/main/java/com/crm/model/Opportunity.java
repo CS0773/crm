6376b776ef3a951e706c9ff2b705e97799c80ad7
shijil.p <shijil.p@claysys.net>
2021-03-09 11:40:09 +0530

    opportunity model

---
+++ src/main/java/com/crm/model/Opportunity.java
@@ -0,0 +1,74 @@
+package com.crm.model;
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "Opportunity")
+public class Opportunity {
+
+    private int id;
+    private String name;
+
+
+
+
+    private Date date;
+    //   private Menmber member;
+
+
+    public Opportunity(int id, String name, Date date) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+    }
+
+    public Opportunity() {
+    }
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+   /* public Date getCloseDate() {
+        return closeDate;
+    }
+
+    public void setCloseDate(Date closeDate) {
+        this.closeDate = closeDate;
+    } */
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Opportunity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+
+                ", date=" + date +
+                '}';
+    }
+
+
+}
