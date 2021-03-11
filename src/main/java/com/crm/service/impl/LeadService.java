df2e5e8aa6d98d5b44dbbfdafee52f3754c5ba31
elizabeth.jose <elizabeth.jose@claysys.com>
2021-03-11 12:53:08 +0530

    lead convert to oppr & member

--- src/main/java/com/crm/service/impl/LeadService.java
+++ src/main/java/com/crm/service/impl/LeadService.java
@@ -2,11 +2,16 @@ package com.crm.service.impl;
 
 import com.crm.model.LeadStatus;
 import com.crm.model.Leads;
+import com.crm.model.Member;
+import com.crm.model.Opportunity;
 import com.crm.service.LeadRepository;
+import com.crm.service.MemberRepository;
+import com.crm.service.OpportunityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -15,7 +20,14 @@ public class LeadService {
 
 	@Autowired
 	private LeadRepository repo;
-	
+
+	@Autowired
+	private MemberRepository memberRepository;
+
+	@Autowired
+	private OpportunityRepository opportunityRepo;
+
+
 	public List<Leads> listAll() {
 		return repo.findAll();
 	}
@@ -34,11 +46,34 @@ public class LeadService {
 
 	@Transactional
 	public void convertLead(int id) {
+
+		// STEP 1 Updating Lead status to CONVERTED
 		Leads leads = new Leads();
 		leads = repo.findById(id).get();
 
 		leads.setStatus(LeadStatus.CONVERTED);
+
+
+		// STEP 3 Create new Member
+		Member member = new Member();
+		member.setAccname(leads.getFirstName() + " " + leads.getLastName() + " Opportunity "+getRandomNumber(1,100));
+		member.setAccno(getRandomNumber(1,100000));
+//		member.setLead(leads);
+		memberRepository.save(member);
+
+		// STEP 2 Create new Opportunity
+		Opportunity opportunity = new Opportunity();
+		opportunity.setName(leads.getFirstName() + " " + leads.getLastName() + " Opportunity "+getRandomNumber(1,100));
+		opportunity.setDate(new Date().toString());
+//		opportunity.setMember(member);
+		opportunityRepo.save(opportunity);
+
 		repo.save(leads);
 
+	}
+
+
+	public int getRandomNumber(int min, int max) {
+		return (int) ((Math.random() * (max - min)) + min);
 	}
 }
